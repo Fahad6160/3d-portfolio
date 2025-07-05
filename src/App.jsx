04431729e382d2d477ea178b425d03ce74b8b896
Fahad Shaikh <fahadshaikh6160@gmail.com>
2025-07-05 23:49:13 +0530

    Implemented Footer Section

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import ShowcaseSection from "./sections/ShowcaseSection"
 import ExperienceSection from "./sections/ExperienceSection"
 import Techstack from "./sections/Techstack"
 import Contact from "./sections/Contact"
+import Footer from "./sections/Footer"
 
 const App = () => {
   return (
@@ -18,6 +19,7 @@ const App = () => {
       <ExperienceSection />
       <Techstack />
       <Contact />
+      <Footer />
     </>
   )
 }
---
+++ src/sections/Footer.jsx
@@ -0,0 +1,29 @@
+import { socialImgs } from "../constants"
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+        <div className="footer-container">
+            <div className="flex flex-col justify-center items-center">
+                <a href="#hero">Back to Top ⬆</a>
+            </div>
+
+            <div className="socials">
+                {socialImgs.map((img)=>(
+                    <a className="icon" target="_blank" href={img.url} key={img.url}>
+                        <img src={img.imgPath} alt={img.name} />
+                    </a>
+                ))}
+            </div>
+
+            <div className="flex flex-col justify-center">
+                <p className="text-center md:text-end ">
+                    &copy; {new Date().getFullYear()} Fahad | Fullstack Developer. All rights reserved.
+                </p>
+            </div>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
