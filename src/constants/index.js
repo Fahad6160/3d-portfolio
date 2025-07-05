451a44b79ccd32081d3fa8695d1c7b31320f168e
Fahad Shaikh <fahadshaikh6160@gmail.com>
2025-07-05 12:45:34 +0530

    Implemented Hero Section

---
+++ src/constants/index.js
@@ -0,0 +1,17 @@
+export const words = [
+    {text: 'Ideas', imgPath: '/images/ideas.svg'},
+    {text: 'Concepts', imgPath: '/images/concepts.svg'},
+    { text: "Designs", imgPath: "/images/designs.svg" },
+    { text: "Code", imgPath: "/images/code.svg" },
+    { text: "Ideas", imgPath: "/images/ideas.svg" },
+    { text: "Concepts", imgPath: "/images/concepts.svg" },
+    { text: "Designs", imgPath: "/images/designs.svg" },
+    { text: "Code", imgPath: "/images/code.svg" }
+]
+
+export const counterItems = [
+  { value: 4, suffix: "+", label: "Years of Experience" },
+  { value: 15, suffix: "+", label: "Techstack Known" },
+  { value: 10, suffix: "+", label: "Completed Projects" },
+  { value: 90, suffix: "%", label: "Website Engagement Rate" },
+];
